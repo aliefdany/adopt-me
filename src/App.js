a7b8b336cf6c3bae99222df6279105b083ec5075
Alief Dany <aliefseventri@gmail.com>
2021-05-02 17:02:19 +0700

    installed npm, prettier, eslint, and .gitignore
    npm used for managing dependencies/packages that this project used.
    in this commit, i added prettier and eslint for code formatting and linting.
    added gitignore for ignoring some files and folder from source control tracking

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 const Pet = (props) => {
-    return React.createElement("div",{}, [
-        React.createElement("h2",{},props.name),
-        React.createElement("h3",{},props.animal),
-        React.createElement("h3",{},props.breed)
-    ]);
-}
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div", 
-        {}, 
-        React.createElement("h1",{},"Adopt Me"),
-        React.createElement(Pet, {name: "Luna", animal: "Dog", breed: "Havanese"}),
-        React.createElement(Pet, {name: "Pepper", animal: "Bird", breed: "Cockatiel"}),
-        React.createElement(Pet, {name: "Sudo", animal: "Dog", breed: "Wheaten Terrier"})
-    );
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaten Terrier",
+    })
+  );
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));

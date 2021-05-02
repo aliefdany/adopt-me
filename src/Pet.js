c24a754f613f3f179ce26fe528e243bb661529e0
Alief Dany <aliefseventri@gmail.com>
2021-05-02 20:53:11 +0700

    converting js code to jsx, fixed eslint fail with react
    in this commit, i convert App.js and Pet.js from pure react to jsx react, eslint fail eventually because configuration for making eslint to work fine with react not yet installed. few plugin installed to fix this problem. they were eslint-plugin-import eslint-plugin-react and eslint-plugin-jsx a11y

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,19 @@
-import React from "react";
+// export default function Pet({ name, animal, breed }) {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, name),
+//     React.createElement("h3", {}, animal),
+//     React.createElement("h3", {}, breed),
+//   ]);
+// }
 
-export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, name),
-    React.createElement("h3", {}, animal),
-    React.createElement("h3", {}, breed),
-  ]);
-}
+const Pet = ({ name, animal, breed }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h3>{animal}</h3>
+      <h3>{breed}</h3>
+    </div>
+  );
+};
+
+export default Pet;

370bc8a9a2a2fd79b8ed69414d0a68e19341129f
Alief Dany <aliefseventri@gmail.com>
2021-05-02 19:49:52 +0700

    extracted Pet component, removing 2 remote unpkg dependencies, add and configuring babel, install react from npm
    Pet component now on separate file called Pet.js, 2 remote dependencies from index.html now removed due to the installation of react from npm, configurig babel for transpilation to support certain browser (last 2 version of chrome)

---
+++ src/Pet.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Pet({ name, animal, breed }) {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, name),
+    React.createElement("h3", {}, animal),
+    React.createElement("h3", {}, breed),
+  ]);
+}
